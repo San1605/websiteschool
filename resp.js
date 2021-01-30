4da84cff17f9d2d72e8282334e6716ddb72a75e1
San1605 <68641395+San1605@users.noreply.github.com>
2021-01-31 00:22:14 +0530

    Add files via upload

---
+++ resp.js
@@ -0,0 +1,20 @@
+sidebar = document.querySelector('.sidebar')
+sidelist = document.querySelector('.sidelist')
+sideheading = document.querySelector('.sideheading')
+
+burger.addEventListener('click', ()=>{
+  
+
+    sidelist.classList.toggle('v-class-resp');
+    sidebar.classList.toggle('h-nav-resp');
+
+
+
+
+
+
+
+
+    
+});
+
